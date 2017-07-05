e24ca09a325d5ccb4cf4c1cfa51ee88f72d63f21
Lê Hồng Phong <phongle6893@gmail.com>
2017-07-05 16:24:05 +0700

    Cau 5

--- LeHongPhong_14110409_cau5.playground/Contents.swift
+++ LeHongPhong_14110409_cau5.playground/Contents.swift
@@ -1,5 +1,41 @@
-//: Playground - noun: a place where people can play
+//
+//  bai5.swift
+//  LeHongPhong_14110409_bai2
+//
+//  Created by PhongLe on 7/5/17.
+//  Copyright © 2017 PhongLe. All rights reserved.
+//
 
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+protocol FullyNamed {
+    var fullName: String { get }
+}
+
+struct Person: FullyNamed {
+    var fullName = ""
+    
+    var age: Int
+}
+
+class XYZ: FullyNamed {
+    
+    var prefix: String?
+    var name: String
+    
+    //DI
+    init(prefix: String, name: String) {
+        self.prefix = prefix
+        self.name = name
+    }
+    var fullName: String {
+        get {
+            if (prefix?.isEmpty)! {
+                return prefix! + " " + name
+            }else{
+                return name
+            }
+        }
+    }
+    
+}
